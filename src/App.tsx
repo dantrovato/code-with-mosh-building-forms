67fffa3fbbb24d5a43a6f3d869ac88fba59c7c1b
Daniele Trovato <plasticsoup@yahoo.co.uk>
2023-04-02 18:36:14 +0100

    Add two form labels, two inputs, a submit button and a handler

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,8 @@
-import { useState } from "react";
-import { produce } from "immer";
+import Form from "./components/Form/Form";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const App = () => {
-  const [cart, setCart] = useState({
-    discount: 1,
-    items: [
-      { id: 1, title: "Product 1", quantity: 1 },
-      { id: 2, title: "Product 2", quantity: 1 },
-    ],
-  });
-
-  const handleClick = () => {
-    setCart({
-      ...cart,
-      items: cart.items.map((item) => {
-        return item.id === 1 ? { ...item, quantity: 2 } : item;
-      }),
-    });
-  };
-
-  return (
-    <div>
-      <ul>
-        {cart.items.map((item) => (
-          <li>
-            {item.title} {item.quantity}
-          </li>
-        ))}
-      </ul>
-      <button onClick={handleClick}>Click</button>
-    </div>
-  );
+  return <Form></Form>;
 };
 
 export default App;
---
+++ src/components/Form/Form.tsx
@@ -0,0 +1,29 @@
+import "./Form.css";
+
+const Form = () => {
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    console.log("submitted");
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="mb-3">
+        <label htmlFor="name" className="form-label">
+          Name
+        </label>
+        <input id="name" type="text" className="form-control" />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="age" className="form-label">
+          Age
+        </label>
+        <input id="age" type="number" className="form-control" />
+      </div>
+      <button className="btn btn-primary" type="submit">
+        Submit
+      </button>
+    </form>
+  );
+};
+
+export default Form;
